dac8ff211a6597595eff233fc000d2424c00c0cf
Tom Stuart <tom@codon.com>
2014-01-24 09:04:12 +0000

    Make both Set tests pass

--- example/lib/set.js
+++ example/lib/set.js
@@ -1,5 +1,12 @@
 var Set = function() {}
 
-Set.prototype.hasMember = function() {
-  return true
+var Set = function(members) {
+  this._members = {}
+  for (var i = 0, n = members.length; i < n; i++) {
+    this._members[members[i]] = true
+  }
+}
+
+Set.prototype.hasMember = function(value) {
+  return this._members.hasOwnProperty(value)
 }
